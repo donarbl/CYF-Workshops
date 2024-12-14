b73bdefc45884be7fdd844c7ff87d0dd722f3e28
AFatmaa <fatma.arslantas@outlook.com>
2024-12-14 15:18:59 +0000

    add

--- dom-merge-conflict/tasks/buttons-and-counter/src/app.js
+++ dom-merge-conflict/tasks/buttons-and-counter/src/app.js
@@ -4,13 +4,20 @@ function increment(node) {
   node.textContent = Number(current) + 1;
 }
 
+function decrement(node) {
+  let current = node.textContent;
+  node.textContent = Number(current) - 1;
+}
+
 export function App() {
   const body = document.createElement("body");
 
   const header = document.createElement("header");
   header.innerHTML = `
         <h1>Number Counter</h1>
-        <p>A simple counter. Press increment to increase the count by one.</p>
+        <p>A simple counter. Press increment to increase the count by one.
+        A simple counter. Press decrement to decrease the count by one.
+        </p>
     `;
   body.appendChild(header);
 
@@ -18,14 +25,20 @@ export function App() {
   main.innerHTML = `
         <p id="counter" data-testid="counter">0</p>
         <button id="increment">Increment</button>
+                <button id="decrement">Decrement</button>
+
     `;
   body.appendChild(main);
 
   const button = body.querySelector("#increment");
+  const decButton = body.querySelector("#decrement");
+
   const counter = body.querySelector("#counter");
   button.addEventListener("click", () => {
     increment(counter);
   });
-
+  decButton.addEventListener("click", () => {
+    decrement(counter);
+  });
   return body;
 }
